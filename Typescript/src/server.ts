d0b9557eb1773f1aabaa5d64e077a2022ad9d2b1
RichardMadrigar <richard_madrigar99@hotmail.com>
2022-01-27 14:59:33 -0300

    att colocando script dev tsc-node

--- Typescript/src/server.ts
+++ Typescript/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.listen(3333, () => {
-  console.log("servidor rodando na porta 3333");
+app.get("/", (req, res) => {
+  console.log("hello");
 });
+app.listen(3333, () => console.log("servidor rodando na porta 3333"));
