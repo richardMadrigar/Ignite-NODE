563c15600ebb5eabb949c51929f8ad7a4282268c
RichardMadrigar <richard_madrigar99@hotmail.com>
2022-01-27 16:34:24 -0300

    add config para usar debug do vsCode

--- Typescript/src/server.ts
+++ Typescript/src/server.ts
@@ -2,7 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   console.log("hello");
 });
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
+});
+
 app.listen(3333, () => console.log("servidor rodando na porta 3333"));
